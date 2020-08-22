d0e5e73f59d8bb75988671cf150960fae0ec81a2
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2020-08-22 06:09:06 -0400

    Inital Setup + Started Docs Tut

---
+++ First Steps/sayHello.gd
@@ -0,0 +1,24 @@
+extends Panel
+
+var totalTime = 0;
+var delay = 1;
+var stopped = false
+
+func _ready():
+	get_node("Button").connect("pressed", self, "_hit_that_button")
+	
+func _hit_that_button():
+	get_node("Label").text = "HELLO!"
+	stopped = true
+	
+func _process(delta):
+	totalTime += delta
+	if !stopped:
+		get_node("Label").text = str(totalTime)
+	else:
+		delay -= delta
+		if delay <= 0:
+			delay = 1
+			stopped = false
+
+# https://docs.godotengine.org/en/stable/getting_started/step_by_step/signals.html
