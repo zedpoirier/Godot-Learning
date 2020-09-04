bcfe3c470404b16a8de1775f73c0d2acabcca55a
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2020-09-04 19:13:00 -0400

    Problem 3 Progress
    Stuck in a super long computation

--- EulerProblems/Problem3.gd
+++ EulerProblems/Problem3.gd
@@ -4,13 +4,30 @@ var largestPrime = 1
 var target = 600851475143
 var test = 13195
 
+var allFactors = []
+var allPrimeFactors = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# find all primes of a number
+	print("\nProblem 3:")
+	# find all factors in target
+	print ("All factors:")
+	for i in range(1, target):
+		if target % i == 0:
+			allFactors.append(i)
+			print(i)
+	# find the primes in the list of factors
+	print("All Primes:")
+	for f in allFactors:
+		if isPrime(f):
+			allPrimeFactors.append(f)
+			print(f)
 	# return largest of those primes
-	pass # Replace with function body.
+	print("Largest Prime:")
+	print(allPrimeFactors[-1])
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func isPrime(num):
+	for i in range(2, num):
+		if num % i == 0:
+			return false
+	return true
