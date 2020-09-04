c5bb9eb740395315e523a97851d9c22bac477256
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2020-09-04 09:08:59 -0400

    Android/HTML Setup & Export
    SUCCESS

--- First Steps/FirstGameAssets/Player.gd
+++ First Steps/FirstGameAssets/Player.gd
@@ -4,31 +4,44 @@ signal hit
 
 export var speed = 400
 var screen_size
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var target = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
+	
 
+func start(pos):
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# define velocity and get vector from input actions
 	# actions can be defined in Project Settings
 	var velocity = Vector2()
-	if Input.is_action_pressed("ui_up"):
-		velocity.y += -1 #godot uses a negative y in 2d
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x += -1
+	
+	# keyboard input has been disabled for exporting to mobile
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y += -1 #godot uses a negative y in 2d
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x += -1
+		
+	# Touch and mouse based input system
+	if position.distance_to(target) > 10:
+		velocity = target - position
+	
 	# check velocity is > 0 and move player
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -54,10 +67,7 @@ func _on_Player_body_entered(body):
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
 	
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+
 	
 	
 	
